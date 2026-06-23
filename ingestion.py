e6247801f8a3d7f5173b2fb41c04dc9fd297161b
Tavo Soto <gsoto@itx.com>
2026-06-23 11:59:26 -0500

    Ingestion logic of text into Pinecone

--- ingestion.py
+++ ingestion.py
@@ -9,3 +9,16 @@ load_dotenv()
 
 if __name__ == '__main__':
     print("Ingesting..")
+    loader = TextLoader("/home/tavo/development/training/ai/langchain-course/mediumblog1.txt")
+    document = loader.load()
+
+    print("splitting...")
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    texts = text_splitter.split_documents(document)
+    print(f"created {len(texts)} chunks")
+
+    embeddings = OpenAIEmbeddings(openai_api_key=os.environ.get("OPENAI_API_KEY"))
+    
+    print("Ingesting..")
+    PineconeVectorStore.from_documents(texts, embeddings, index_name=os.environ.get("INDEX_NAME"))
+    print("Finished")
